70a42d768082254284e9fb8da547841a0d565669
My Name <someone@email.com>
2024-04-20 12:57:42 +0500

    TextAnalyzer

---
+++ src/components/About.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // }) 
+    let myStyle = {
+        color: props.mode ==='dark'?'white':'#042743',
+        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
+    }
+    
+    return (
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>Text Crafter </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    TextCase Analyzer gives you a way to analyze your text quickly and efficiently. 
+
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <strong>Features </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    TextCase Analyzer is used to convert the letter from upper to lower case  and vice versa.
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>Cross-browser compatibility </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    This word counting software functions seamlessly on various web browsers, including Chrome, Firefox, Internet Explorer, Safari, and Opera. It is versatile enough to accurately count characters across platforms such as Facebook, blogs, books, Excel documents, PDFs, essays, and more.
+
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
---
+++ src/components/SocialAccounts.js
@@ -0,0 +1,35 @@
+// SocialAccounts.js
+
+import React from 'react';
+import './SocialAccounts.css'; // Import the CSS file for styling
+
+export default function SocialAccounts() {
+  return (
+    <div className="social-accounts">
+      <h3></h3>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/mubashirali8/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i> LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/mubashir1837" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i> GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://instagram.com/your_instagram_username" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-instagram"></i> Instagram
+          </a>
+        </li>
+        <li>
+          <a href="https://facebook.com/your_facebook_username" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-facebook"></i> Facebook
+          </a>
+        </li>
+        {/* Add more social media accounts as needed */}
+      </ul>
+    </div>
+  );
+}
