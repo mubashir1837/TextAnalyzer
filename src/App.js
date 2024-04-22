c07def8d9678092472c78251962ba276db974a9a
My Name <someone@email.com>
2024-04-22 18:53:50 +0500

    portfolio

--- src/App.js
+++ src/App.js
@@ -32,18 +32,18 @@ function App() {
   const toggleMode = () => {
     if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+      document.body.style.backgroundColor = '#2f0443';
       showAlert("Dark mode has been enabled", "success");
     } else {
       setMode('light');
-      document.body.style.backgroundColor = 'white';
+      document.body.style.backgroundColor = '#19bdaf';
       showAlert("Light mode has been enabled", "success");
     }
   }
 
   return (
     <Router>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Navbar title="Text Aalyzer" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my-3">
         <Switch>
@@ -51,7 +51,7 @@ function App() {
             <About mode={mode} />
           </Route>
           <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />
+            <TextForm showAlert={showAlert} heading="Text Analyzer - character counter, word counter, remove extra spaces" mode={mode} />
           </Route>
         </Switch>
       </div>
